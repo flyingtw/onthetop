a554119968b425c6c899dde513670fa7ac4c969c
SeokHyun <SeokHyun@DESKTOP-K5NDV2B>
2020-06-03 13:46:33 +0900

    DB연동 및 데이터 조회 성공

--- OnTheTop/src/main/java/com/onthetop/Controller/BoardController.java
+++ OnTheTop/src/main/java/com/onthetop/Controller/BoardController.java
@@ -2,14 +2,10 @@ package com.onthetop.Controller;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Timestamp;
-import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.onthetop.domain.Board;
-import com.onthetop.domain.PageInfo;
 import com.onthetop.service.BoardService;
 
 @Controller
@@ -33,36 +27,9 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 
-	@RequestMapping
-	public String list(@RequestParam(value = "pageNum", required = false, defaultValue = "1") int pageNum,
-			Model model) {
-
-		int count = boardService.getBoardCount();
-
-		int pageSize = 15;
-		int startRow = (pageNum - 1) * pageSize + 1;
-		int endRow = pageNum * pageSize;
-
-		List<Board> list = boardService.getBoardList(startRow, pageSize);
-
-		int pageCount = count / pageSize + (count % pageSize == 0 ? 0 : 1);
-		int pageBlock = 10;
-		int startPage = ((pageNum / pageBlock) - (pageNum % pageBlock == 0 ? 1 : 0)) * pageBlock + 1;
-		int endPage = startPage + pageBlock - 1;
-		if (endPage > pageCount)
-			endPage = pageCount;
-
-		PageInfo pageInfo = new PageInfo();
-		pageInfo.setCount(count);
-		pageInfo.setPageNum(pageNum);
-		pageInfo.setPageCount(pageCount);
-		pageInfo.setPageBlock(pageBlock);
-		pageInfo.setStartPage(startPage);
-		pageInfo.setEndPage(endPage);
-
-		model.addAttribute("list", list);
-		model.addAttribute("pageInfo", pageInfo);
-
+	@RequestMapping(value = "list")
+	public String boardList(Model model) {
+		model.addAttribute("list", boardService.getBoardList());
 		return "board/list";
 	}
 
@@ -96,7 +63,7 @@ public class BoardController {
 
 			IOUtils.copy(multipartFile.getInputStream(), new FileOutputStream(file));
 		} else {
-			System.out.println("파일이 존재하지 않거나 파일크기가 0 입니다.");
+			System.out.println("파일이  존재하지 않거나 파일크기가 0 입니다.");
 		}
 
 		board.setFilename(filename);
@@ -109,73 +76,133 @@ public class BoardController {
 		return "redirect:/board/list";
 	}
 
-	@RequestMapping("detail")
-	public String detail(@RequestParam int num, Model model) {
-
-		boardService.updateReadCount(num);
-		Board board = boardService.getBoard(num);
-
-		model.addAttribute("board", board);
-
-		return "board/content";
-	}
-
-	@RequestMapping(value = "update", method = RequestMethod.GET)
-	public String update(@RequestParam int num, Model model) {
-		Board board = boardService.getBoard(num);
-
-		model.addAttribute("board", board);
-
-		return "board/updateForm";
-	}
-
-	@RequestMapping(value = "update", method = RequestMethod.POST)
-	public ModelAndView update(@ModelAttribute Board board, @RequestParam String pageNum, HttpServletResponse response)
-			throws IOException {
-		int check = boardService.updateBoard(board);
-
-		if (check == 0) { // 패스워듭 불일치로 수정 실패
-			response.setContentType("text/html; charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.println("<script>");
-			out.println("alert('패스워드틀림');");
-			out.println("history.back();");
-			out.println("</script>");
-			out.close();
-			return null;
-		}
-
-		ModelAndView mav = new ModelAndView("redirect:/board/list");
-		mav.addObject("pageNum", pageNum);
-		return mav;
-
-	}
-
-	@RequestMapping(value = "delete", method = RequestMethod.GET)
-	public String delete() {
-		return "board/deleteForm";
-	}
-
-	@RequestMapping(value = "delte", method = RequestMethod.POST)
-	public ModelAndView delete(@RequestParam int num, @RequestParam String passwd, @RequestParam String pageNum,
-			HttpServletResponse response) throws IOException {
-		int check = boardService.deleteBoard(num, passwd);
-
-		if (check == 0) { // 패스워듭 불일치로 수정 실패
-			response.setContentType("text/html; charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.println("<script>");
-			out.println("alert('패스워드틀림');");
-			out.println("history.back();");
-			out.println("</script>");
-			out.close();
-			return null;
-		}
-
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("redirect:/board/list");
-		mav.addObject("pageNum", pageNum);
-		return mav;
-	}
+	/**
+	 * @RequestMapping public String list(@RequestParam(value = "pageNum", required
+	 *                 = false, defaultValue = "1") int pageNum, Model model) {
+	 * 
+	 *                 int count = boardService.getBoardCount();
+	 * 
+	 *                 int pageSize = 15; int startRow = (pageNum - 1) * pageSize +
+	 *                 1; int endRow = pageNum * pageSize;
+	 * 
+	 *                 List<Board> list = boardService.getBoardList(startRow,
+	 *                 pageSize);
+	 * 
+	 *                 int pageCount = count / pageSize + (count % pageSize == 0 ? 0
+	 *                 : 1); int pageBlock = 10; int startPage = ((pageNum /
+	 *                 pageBlock) - (pageNum % pageBlock == 0 ? 1 : 0)) * pageBlock
+	 *                 + 1; int endPage = startPage + pageBlock - 1; if (endPage >
+	 *                 pageCount) endPage = pageCount;
+	 * 
+	 *                 PageInfo pageInfo = new PageInfo(); pageInfo.setCount(count);
+	 *                 pageInfo.setPageNum(pageNum);
+	 *                 pageInfo.setPageCount(pageCount);
+	 *                 pageInfo.setPageBlock(pageBlock);
+	 *                 pageInfo.setStartPage(startPage);
+	 *                 pageInfo.setEndPage(endPage);
+	 * 
+	 *                 model.addAttribute("list", list);
+	 *                 model.addAttribute("pageInfo", pageInfo);
+	 * 
+	 *                 return "board/list"; }
+	 * 
+	 * @RequestMapping(value = "add", method = RequestMethod.GET) public String
+	 *                       add() { return "board/fwriteForm"; }
+	 * 
+	 * @RequestMapping(value = "add", method = RequestMethod.POST) public String
+	 *                       add(HttpServletRequest request, @RequestParam("file")
+	 *                       MultipartFile multipartFile,
+	 * @ModelAttribute Board board) throws Exception {
+	 * 
+	 *                 String filename = null; if (!multipartFile.isEmpty()) { //
+	 *                 업로드한 파일이 있을 때 ServletContext application =
+	 *                 request.getServletContext(); String realPath =
+	 *                 application.getRealPath("/upload");
+	 * 
+	 *                 filename = multipartFile.getOriginalFilename();
+	 * 
+	 *                 int index = filename.lastIndexOf("\\"); filename =
+	 *                 filename.substring(index + 1);
+	 * 
+	 *                 File file = new File(realPath, filename); if (file.exists())
+	 *                 { filename = System.currentTimeMillis() + "_" + filename;
+	 *                 file = new File(realPath, filename); }
+	 * 
+	 *                 System.out.println("업로드 경로: " + realPath);
+	 *                 System.out.println("업로드 파일명: " + filename);
+	 * 
+	 *                 IOUtils.copy(multipartFile.getInputStream(), new
+	 *                 FileOutputStream(file)); } else { System.out.println("파일이
+	 *                 존재하지 않거나 파일크기가 0 입니다."); }
+	 * 
+	 *                 board.setFilename(filename);
+	 * 
+	 *                 board.setReg_date(new Timestamp(System.currentTimeMillis()));
+	 *                 board.setIp(request.getRemoteAddr());
+	 * 
+	 *                 boardService.add(board);
+	 * 
+	 *                 return "redirect:/board/list"; }
+	 * 
+	 *                 @RequestMapping("detail") public String detail(@RequestParam
+	 *                 int num, Model model) {
+	 * 
+	 *                 boardService.updateReadCount(num); Board board =
+	 *                 boardService.getBoard(num);
+	 * 
+	 *                 model.addAttribute("board", board);
+	 * 
+	 *                 return "board/content"; }
+	 * 
+	 * @RequestMapping(value = "update", method = RequestMethod.GET) public String
+	 *                       update(@RequestParam int num, Model model) { Board
+	 *                       board = boardService.getBoard(num);
+	 * 
+	 *                       model.addAttribute("board", board);
+	 * 
+	 *                       return "board/updateForm"; }
+	 * 
+	 * @RequestMapping(value = "update", method = RequestMethod.POST) public
+	 *                       ModelAndView update(@ModelAttribute Board
+	 *                       board, @RequestParam String pageNum,
+	 *                       HttpServletResponse response) throws IOException { int
+	 *                       check = boardService.updateBoard(board);
+	 * 
+	 *                       if (check == 0) { // 패스워듭 불일치로 수정 실패
+	 *                       response.setContentType("text/html; charset=UTF-8");
+	 *                       PrintWriter out = response.getWriter();
+	 *                       out.println("<script>");
+	 *                       out.println("alert('패스워드틀림');");
+	 *                       out.println("history.back();");
+	 *                       out.println("</script>"); out.close(); return null; }
+	 * 
+	 *                       ModelAndView mav = new
+	 *                       ModelAndView("redirect:/board/list");
+	 *                       mav.addObject("pageNum", pageNum); return mav;
+	 * 
+	 *                       }
+	 * 
+	 * @RequestMapping(value = "delete", method = RequestMethod.GET) public String
+	 *                       delete() { return "board/deleteForm"; }
+	 * 
+	 * @RequestMapping(value = "delte", method = RequestMethod.POST) public
+	 *                       ModelAndView delete(@RequestParam int
+	 *                       num, @RequestParam String passwd, @RequestParam String
+	 *                       pageNum, HttpServletResponse response) throws
+	 *                       IOException { int check = boardService.deleteBoard(num,
+	 *                       passwd);
+	 * 
+	 *                       if (check == 0) { // 패스워듭 불일치로 수정 실패
+	 *                       response.setContentType("text/html; charset=UTF-8");
+	 *                       PrintWriter out = response.getWriter();
+	 *                       out.println("<script>");
+	 *                       out.println("alert('패스워드틀림');");
+	 *                       out.println("history.back();");
+	 *                       out.println("</script>"); out.close(); return null; }
+	 * 
+	 *                       ModelAndView mav = new ModelAndView();
+	 *                       mav.setViewName("redirect:/board/list");
+	 *                       mav.addObject("pageNum", pageNum); return mav; }
+	 **/
 
 }
--- OnTheTop/src/main/java/com/onthetop/service/BoardService.java
+++ OnTheTop/src/main/java/com/onthetop/service/BoardService.java
@@ -4,74 +4,22 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.onthetop.domain.Board;
 import com.onthetop.repository.BoardDao;
 
 @Service
-@Transactional
 public class BoardService {
 
 	@Autowired
 	private BoardDao boardDao;
 
-	public void add(Board board) {
-		Integer maxNum = boardDao.getMaxNum();
-		if (maxNum == null) {
-			board.setNum(1);
-		} else {
-			board.setNum(maxNum + 1);
-		}
-
-		board.setRe_ref(board.getNum());
-		board.setRe_lev(0);
-		board.setRe_seq(0);
-		board.setReadcount(0);
-
-		boardDao.add(board);
+	public List<Board> getBoardList() {
+		return boardDao.getBoardList();
 	}
 
-	public List<Board> getBoardList(int startRow, int pageSize) {
-		return boardDao.getBoardList(startRow, pageSize);
-	}
-
-	public int getBoardCount() {
-		return boardDao.count();
-	}
-
-	public void updateReadCount(int num) {
-		boardDao.updateReadCount(num);
-	}
-
-	public Board getBoard(int num) {
-		return boardDao.getBoard(num);
-	}
-
-	public int updateBoard(Board board) {
-		Board boardDB = boardDao.getBoard(board.getNum());
-
-		int check = 0;
-		if (board.getPasswd().equals(boardDB.getPasswd())) {
-			boardDao.updateBoard(board);
-			check = 1;
-		} else {
-			check = 0;
-		}
-		return check;
-	}
-	
-	public int deleteBoard(int num, String passwd) {
-		Board boardDB = boardDao.getBoard(num);
-
-		int check = 0;
-		if (passwd.equals(boardDB.getPasswd())) {
-			boardDao.deleteBoard(num);
-			check = 1;
-		} else {
-			check = 0;
-		}
-		return check;
+	public Board add(Board board) {
+		return boardDao.insertBoard();
 	}
 
 }
